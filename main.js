df58e8bed3e71eca50f4f5c849a5e34fb5c708a2
Patrick-orz <patrick_tang@outlook.com>
2024-04-13 16:14:07 -0400

    feat(tray icon): add cat tray icon

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow, ipcMain } = require('electron')
+const { Tray, Menu, nativeImage } = require('electron/main')
 const path = require('node:path')
 
 const createWindow = () => {
@@ -13,7 +14,21 @@ const createWindow = () => {
     win.loadFile('index.html')
 }
 
-app.whenReady().then(() => {
+let tray
+
+app.whenReady().then(async() => {
+    const icon = await nativeImage.createThumbnailFromPath("../catPlate/Icon/icons8-cat-64.jpg",{ width: 19, height: 19 })
+    tray = new Tray(icon)
+
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'Item1', type: 'radio' },
+        { label: 'Item2', type: 'radio' },
+        { label: 'Item3', type: 'radio', checked: true },
+        { label: 'Item4', type: 'radio' }
+    ])
+
+    tray.setToolTip('This is my application.')
+    tray.setContextMenu(contextMenu)
     ipcMain.handle('ping', () => 'pong')
     createWindow()
     app.on('activate', () => {
