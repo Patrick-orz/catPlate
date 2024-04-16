3fe96025f9c6bd21b739b47bc16f5f46dc03270a
Patrick-orz <patrick_tang@outlook.com>
2024-04-16 19:59:09 -0400

    feat(plate): add prototype editing toggle

--- src/js/renderer.js
+++ src/js/renderer.js
@@ -1,17 +1,41 @@
-// textArea
-const textInput = document.getElementById('textInput');
+// Fetch elements
+const mdInput = document.getElementById("markdown-input");
+const mdDisplay = document.getElementById("markdown-display");
+const editButton = document.getElementById("edit-button");
 
-// load saved plate
 async function initialize() {
-    textInput.value = await plate.loadPlate();
+    // load saved plate
+    mdInput.value = await plate.loadPlate();
+    mdDisplay.innerHTML = mdInput.value.replace(/\n/g, "<br>");
+
+    // hide editing
+    mdInput.style.display = "none";
 }
 initialize()
+
+// toggle edit button
+function toggleMarkdownEdit() {
+    if (mdInput.style.display === "none") {
+        // Show the input area and hide the display area
+        mdInput.style.display = "block";
+        mdDisplay.style.display = "none";
+        editButton.textContent = "View Markdown";
+    } else {
+        // Show the display area and hide the input area
+        mdInput.style.display = "none";
+        mdDisplay.style.display = "block";
+        mdDisplay.innerHTML = mdInput.value.replace(/\n/g, "<br>");
+        editButton.textContent = "Edit Markdown";
+    }
+}
+editButton.addEventListener("click", toggleMarkdownEdit);
+
 
 // Listen for save command: CTRL+S (win) or META+S (mac)
 document.addEventListener('keydown', e => {
     if ((e.ctrlKey || e.metaKey) && e.key === 's') {
         console.log("Save");
         // Call save
-        plate.savePlate(textInput.value);
+        plate.savePlate(mdInput.value);
     }
 });
