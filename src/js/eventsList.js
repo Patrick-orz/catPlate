bf272289acee0f103ae7fd787ffdbc28b6167ad9
Patrick-orz <patrick_tang@outlook.com>
2024-04-25 15:42:01 -0400

    feat(plate): add event removal todo: add sorting & overdue

--- src/js/eventsList.js
+++ src/js/eventsList.js
@@ -2,6 +2,8 @@
 {/*     <div class="col-7 d-flex justify-content-center align-items-center"> */}
 {/*     </div> */}
 {/*     <div class="col-2 d-flex justify-content-center align-items-center"> */}
+{/*         <span class="black-highight"> */}
+{/*         </span> */}
 {/*         <span class=""> */}
 {/*         </span> */}
 {/*     </div> */}
@@ -14,6 +16,20 @@
 {/*     </div> */}
 {/* </div> */}
 
+async function handleRemoval(){
+    // deletion confirmed
+    $(".confirmDelete-input").click(async function(){
+        let events = await plate.loadPlate("events");
+        // console.log($(this).index('.confirmDelete-input'));
+        events.splice($(this).index('.confirmDelete-input'), 1);
+        console.log(events);
+
+        plate.savePlate("events", events);
+        // Tell other js file that there's pending plates change
+        $("#js-communication").trigger("pending-plates-change");
+
+    })
+}
 async function updatePlate() {
     // init
     let events = await plate.loadPlate("events");
@@ -36,24 +52,47 @@ async function updatePlate() {
                 description +
                 '</div>' +
                 '<div class="col-2 d-flex justify-content-center align-items-center">' +
-                '<span class="' + highlights[priority] + '">' +
+                    '<span class="' + highlights[priority] + '">' +
                 priorities[priority] +
-                '</span>' +
+                    '</span>' +
                 '</div>' +
                 '<div class="col-2 d-flex justify-content-center align-items-center">' +
-                '<span class="black-highlight">' +
+                    '<span class="black-highlight">' +
                 'DUE: ' + due +
-                '</span>' +
+                    '</span>' +
                 '</div>' +
-                '<div class="col-1 d-flex justify-content-center align-items-center">' +
-                '<i class="fa-solid fa-trash-can text-danger"></i>' +
-                '</div>' +
+                    '<div class="col-1 d-flex justify-content-center align-items-center removal-container">' +
+                        '<button type="button" class="btn deleteEvent-input" data-bs-toggle="modal" data-bs-target="#' + 'eventDeletionConfirmation' + i +'">' +
+                            '<i class="fa-solid fa-trash-can text-danger"></i>' +
+                        '</button>' +
+
+// confirmation modal
+'<div class="modal fade" id="' + 'eventDeletionConfirmation' + i + '"" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="' + 'eventDeletionConfirmation' + i + 'Label' + '" aria-hidden="true">' +
+'  <div class="modal-dialog modal-dialog-centered">' +
+'    <div class="modal-content">' +
+'      <div class="modal-header">' +
+'        <h5 class="modal-title" id="' + 'eventDeletionConfirmation' + i +'Label' + '">Are you sure about deleting this event?</h5>' +
+'        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>' +
+'      </div>' +
+'      <div class="modal-body">' +
+'It would be lost forever!' +
+'      </div>' +
+'      <div class="modal-footer">' +
+'        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>' +
+'        <button type="button" class="btn btn-danger confirmDelete-input" data-bs-dismiss="modal">Delete</button>' +
+'      </div>' +
+'    </div>' +
+'  </div>' +
+'</div>' +
+
+                    '</div>' +
                 '</div>' +
                 '<hr>';
 
         // push into view
         container.append(event);
     }
+    handleRemoval();
 
     // Note to self: stay consistent with JQuery and vanilla JS!
 }
